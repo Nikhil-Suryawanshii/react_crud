49678c42024b6dd16db9c4d9c047a95b38458734
8806721951@nikhil <nikhilsuryawanshi000@gmail.com>
2025-01-07 11:33:21 +0530

    small change to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",             // Root HTML file
+    "./src/**/*.{js,jsx,ts,tsx}", // Source files for React components
   ],
   theme: {
     extend: {},
